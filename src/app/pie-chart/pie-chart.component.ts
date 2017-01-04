004b2a6fe700da441a49a85719cfc7a157a4f810
menghong chhay <chhaymenghong@yahoo.com>
2017-01-04 01:52:13 -0800

    add pie chart component

---
+++ src/app/pie-chart/pie-chart.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.css'],
+  host: {class: 'row'}
+})
+export class PieChartComponent implements OnInit {
+  public options;
+  constructor( private _searchService: SearchService ) {
+  }
+
+  ngOnInit() {
+    this._searchService.data$.subscribe( data => this._createChartOption( data ) );
+
+
+  }
+  private _createChartOption( data ) {
+    this.options = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Languages'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data : data
+        }]
+      }
+  }
+
+}

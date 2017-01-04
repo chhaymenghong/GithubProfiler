c9dee96211a40d8ce46fd6c47877f7ad6715ef55
menghong chhay <chhaymenghong@yahoo.com>
2017-01-04 01:53:53 -0800

    add some config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ChartModule } from 'angular2-highcharts';
 
 import { AppComponent } from './app.component';
 import { LogoComponent } from './logo/logo.component';
+import { SearchComponent } from './search/search.component';
+import { SearchService } from './search.service';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    SearchComponent,
+    UserComponent,
+    RepositoryComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartModule
   ],
-  providers: [],
+  providers: [ SearchService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

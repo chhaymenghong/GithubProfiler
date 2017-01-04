e3ce61366f7f2d2755a71db79cedfae0c71058ff
menghong chhay <chhaymenghong@yahoo.com>
2017-01-04 01:52:21 -0800

    add env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  clientId: '0a6157420ccf24836062',
+  clientSecret: '369d1e34bf674236718c338a923c2bfb65930c50',
+  baseUrl: 'https://api.github.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  clientId: '0a6157420ccf24836062',
+  clientSecret: '369d1e34bf674236718c338a923c2bfb65930c50',
+  baseUrl: 'https://api.github.com/'
 };

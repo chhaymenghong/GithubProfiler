05732a469c385401fa99f46bc5e14de3c29dd5b3
menghong chhay <chhaymenghong@yahoo.com>
2017-01-04 22:49:58 -0800

    get rid of config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  clientId: '0a6157420ccf24836062',
-  clientSecret: '369d1e34bf674236718c338a923c2bfb65930c50',
   baseUrl: 'https://api.github.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,5 @@
 
 export const environment = {
   production: false,
-  clientId: '0a6157420ccf24836062',
-  clientSecret: '369d1e34bf674236718c338a923c2bfb65930c50',
   baseUrl: 'https://api.github.com/'
 };

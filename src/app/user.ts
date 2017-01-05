fc1892c164fe85ae10f2c0c44f25602bb45ce9b4
menghong chhay <chhaymenghong@yahoo.com>
2017-01-04 17:34:43 -0800

    add spinner tag to repo and user views

--- src/app/user.ts
+++ src/app/user.ts
@@ -7,14 +7,15 @@ export class User {
   public location: string;
   public profileUrl: string;
   public memberSince: string
+  public readonly UNKNOWN: string = 'Unknown';
 
   constructor( json: any ) {
-    this.name = json.name;
+    this.name = json.name || this.UNKNOWN;
     this.avatar_url = json.avatar_url;
-    this.email = json.email;
+    this.email = json.email || this.UNKNOWN;;
     this.followers = json.followers;
     this.following = json.following;
-    this.location = json.location;
+    this.location = json.location || this.UNKNOWN;;
     this.profileUrl = json.html_url;
     this.memberSince = json.created_at;
   }
